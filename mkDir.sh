225f5a81dd6f5160843047ce3a4284a249c1d182
matthewc2003 <matthewc2003@gmail.com>
2021-07-04 17:17:28 -0700

    filename fix, formatting updates

--- mkDir.sh
+++ mkDir.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#Copyright 2021, Matthew Chung, All rights reserved.
-
+#
+# Copyright 2021, Matthew Chung, All rights reserved.
+#
 # generates the 20 directories
-
+#
 for i in {01..20}
 do
-mkdir matthew_${i}
-
+	mkdir matthew_${i}
 done
+
 # generates the last one
-printf "done"
+echo "done"
